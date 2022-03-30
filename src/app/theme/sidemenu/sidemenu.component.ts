1ec5427d025bde0a87a33859cdf955bd986d2d94
primo <primo@u-gee.com>
2022-03-30 17:21:23 +0800

    feat(tab): f5 後 tab 名稱對應正確

--- src/app/theme/sidemenu/sidemenu.component.ts
+++ src/app/theme/sidemenu/sidemenu.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { Menu, MenuService } from '@core/bootstrap/menu.service';
 import { SidenavRouteInterface, TabService } from '@shared/services/tab.service';
 
@@ -15,10 +16,75 @@ export class SidemenuComponent {
   menu$ = this.menu.getAll();
   buildRoute = this.menu.buildRoute;
 
+  private flattenDeep(arr1: any) {
+    return arr1.reduce((acc: any, val: any) => {
+      // console.log(val.items)
+      return Array.isArray(val.children)
+        ? acc.concat(this.flattenDeep(val.children))
+        : acc.concat(val);
+    }, []);
+  }
+
   public tabs: SidenavRouteInterface[] = [];
 
-  constructor(private menu: MenuService, private tabService: TabService) {
+  constructor(
+    private menu: MenuService,
+    public router: Router,
+    // private route:ActivatedRoute,
+    private tabService: TabService
+  ) {
     this.tabs = tabService.tabs;
+
+    //*****************
+    let isNotContain = true;
+    this.tabService.tabs.forEach(tab => {
+      if (tab.link === this.router.url) {
+        isNotContain = false;
+      }
+    });
+    // console.log(isNotContain)
+    // console.log(this.router.url)
+    // console.log(this.tabService.tabs)
+    // console.log(this.appMenuComponent.model)
+    // https://developer.mozilla.org/zh-TW/docs/Web/JavaScript/Reference/Global_Objects/Array/flat#browser_compatibility
+
+    if (isNotContain) {
+      // this.router.events
+      //    .subscribe((event) => {
+      //      console.log('this is what your looking for ', event);
+      //   });
+
+      // console.log(this.route.snapshot.pathFromRoot);
+
+      this.menu$.subscribe(value => {
+        // console.log(value);
+        const tabArray = this.flattenDeep(value);
+        // console.log(tabArray);
+        // console.log(this.router.url);
+        const tabArray2 = tabArray.filter(
+          (e: { route: string }) => e.route && this.router.url.endsWith(e.route)
+        );
+        // console.log(tabArray2[0]);
+        // console.log(tabArray2[0].name);
+        this.tabs.push({ name: tabArray2[0].name, link: this.router.url });
+      });
+
+      // console.log(this.router.config);
+      // const tabArray = this.flattenDeep(this.router.config);
+      // console.log(tabArray);
+      // const tabArray2 = tabArray.filter((e: { routerLink: string[]; }) => e.routerLink && e.routerLink[0] === this.router.url);
+      // console.log(tabArray2);
+      // console.log(tabArray2[0].label)
+      // console.log(tabArray2[0].routerLink)
+
+      // this.tabService.tabs.push({ label: tabArray2[0].label, link: this.router.url ,isVisible: true, isDisabled: false});
+      // setTimeout(() => {
+      //       this.tabService.selectedIndex = this.tabService.tabs.length - 1
+      //   }, 200);
+      // console.log(this.router.config);
+    }
+
+    //*****************
   }
 
   // changeRoute(name: string, link: string): void {
